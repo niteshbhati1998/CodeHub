09b913adb4e4ae02b048633e62ca86b7524d4b5e
niteshbhati1998 <niteshbhati1998@gmail.com>
2026-02-15 14:09:54 +0530

    testcontainer integration testing

--- Test/src/test/java/com/example/test/integration/controller/MyControllerIT.java
+++ Test/src/test/java/com/example/test/integration/controller/MyControllerIT.java
@@ -7,12 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 class MyControllerIT {
 
     @Autowired
@@ -20,6 +26,17 @@ class MyControllerIT {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Container
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:18");
+
+    @DynamicPropertySource
+    static void dynamicConfiguration(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
     @Test
     void shouldSaveUser() throws Exception {
@@ -38,6 +55,19 @@ class MyControllerIT {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("User saved successfully"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.payload.name").value("nitesh"));
+    }
+
+    @Test
+    void shouldThrowErrorOnException() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/user/save")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new User()))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Failed to save user"));
+
     }
 
 }
